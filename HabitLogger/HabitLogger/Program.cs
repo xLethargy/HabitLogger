51aa7b6d4237e0a6c22d14b049d8250ed8083c8c
lethargy <qaimk@rocketmail.com>
2023-12-02 14:04:48 +0000

    SQLite Database Creation

--- HabitLogger/HabitLogger/Program.cs
+++ HabitLogger/HabitLogger/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace HabitLogger
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace HabitLogger
 {
     internal class Program
     {
         static void Main()
         {
-            
+
+            string connectionString = @"Data Source=..\..\..\Files\Library.db;Version=3;";
+
+            if (!File.Exists(@"..\..\..\Files\Library.db"))
+            {
+                SQLiteConnection.CreateFile(@"..\..\..\Files\Library.db");
+
+
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    var tableCmd = connection.CreateCommand();
+
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS drinking_water (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Quantity INTEGER
+                    )";
+
+                    tableCmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
         }
     }
 }
